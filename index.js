7656c26ebe44020bdfec96e0a186f33a08bceb07
Haider Mehdi <haidershah659@gmail.com>
2023-10-19 00:36:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+let Rivers = ["Amazon", "Yangtze", "Nile", "Mississippi", "Congo"];
+console.log(`List of top Rivers in the World`);
+for (let river of Rivers) {
+    console.log(river);
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+let Rivers = ["Amazon", "Yangtze", "Nile", "Mississippi", "Congo" ]
+
+console.log(`List of top Rivers in the World`)
+for (let river of Rivers ) {
+    console.log(river)
+}
+export{};
